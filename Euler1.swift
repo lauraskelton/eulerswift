ae11d6f7a3d28c13778c7a7b3e5a5ba7a21bb27c
Laura Skelton <lauraeskelton@gmail.com>
2014-07-29 14:24:36 -0400

    problem 2

---
+++ Euler1.swift
@@ -0,0 +1,9 @@
+//
+//  Euler1.swift
+//  
+//
+//  Created by Laura Skelton on 7/29/14.
+//
+//
+
+import Foundation
---
+++ Euler2.swift
@@ -0,0 +1,9 @@
+//
+//  Euler2.swift
+//  
+//
+//  Created by Laura Skelton on 7/29/14.
+//
+//
+
+import Foundation
